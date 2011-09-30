8b9fe268380ed8f4bed5de95c7089154e580a101
He Jun <freefcw@gmail.com>
2011-09-30 23:02:15 +0800

    add the contact page

---
+++ application/views/index/contact.php
@@ -0,0 +1,15 @@
+<h1>Contact Us</h1>
+
+<dl>
+<dt>About problems</dt>
+<dd>If you have question on the problems, please discuss at <?php echo html::anchor('/discuss', 'Discuss');?>, or you can contact freefcw@gmail.com</dd>
+<dt>About the Online Judge System</dt>
+<dd>If you are interested in the Online Judge System, you can get the source code from <?php echo html::anchor('http://code.google.com/p/hustoj', 'Google Code'); ?></dd>
+<dt>help Center</dt>
+<dd>Any other problems please contact freefcw@gmail.com</dd>
+</dl>
+
+<div id="join">
+	if you interested in ACM/ICPC, 
+	<?php echo html::anchor('/joinus', 'Join us!')?>
+</div>
