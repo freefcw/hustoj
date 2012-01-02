f616e9d1c2f11d8809e23cb7f2fea14d02ce12e0
He Jun <freefcw@gmail.com>
2012-01-02 19:08:30 +0800

    fix contest nav

--- application/views/contest/nav.php
+++ application/views/contest/nav.php
@@ -1,4 +1,4 @@
-<ul class="inline-ul">
+<ul class="pills contest-nav">
 <li><?php echo html::anchor("/contest/show/{$cid}", 'Problems');?></li>
 <li><?php echo html::anchor("/contest/standing/{$cid}", 'Standing');?></li>
 <li><?php echo html::anchor("/contest/statics/{$cid}", 'Statistics');?></li>
