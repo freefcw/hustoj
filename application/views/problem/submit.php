9e00b588b85607b867c59ac10aa5140aeca2e55f
He Jun <freefcw@gmail.com>
2012-01-06 19:32:28 +0800

    change username to user_id add language choose

--- application/views/problem/submit.php
+++ application/views/problem/submit.php
@@ -3,9 +3,11 @@
 <fieldset>
 <?php echo Form::label('pid', "Problem ID : ");?>
 <?php echo Form::input('pid', $pid); ?>
+<?php echo Form::label('pid', "Language : ");?>
+<?php echo Form::select('language', array('0'=>'C', '1'=>'C++', '2'=>'Pascal', '3'=>'Java'));?>
 <?php if (Auth::instance()->get_user() == null):?>
-    <?php echo Form::label('username', "Username : ");?>
-    <?php echo Form::input('username', Cookie::get('username', '')); ?>
+    <?php echo Form::label('user_id', "Username : ");?>
+    <?php echo Form::input('user_id', Cookie::get('use_id', '')); ?>
     <?php echo Form::label('password', "Password : ");?>
     <?php echo Form::input('password'); ?>
 <?php endif;?>
