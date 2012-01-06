88655c2cc56936657534a837be6d07d4cddd38da
He Jun <freefcw@gmail.com>
2012-01-07 02:26:15 +0800

    delete log file

--- application/logs/2011/09/17.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2011-09-17 22:08:07 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH\classes\kohana\request.php [ 1126 ]
-2011-09-17 22:08:07 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH\classes\kohana\request.php [ 1126 ]
---
-#0 D:\workspace\hustoj\index.php(109): Kohana_Request->execute()
-#1 {main}
