c7a00032ba52c24488182a4d7813dfc50dfd661a
He Jun <freefcw@gmail.com>
2012-01-10 01:18:44 +0800

    change Model_Problem to Mongodb,split about submission to Model_submission

--- application/classes/model/problem.php
+++ application/classes/model/problem.php
@@ -11,38 +11,21 @@ class Model_Problem extends Model_Mongo {
     {
         $collection = $this->db->selectCollection('problem');
 
-        $ret = $collection->findOne(array('problem_id' => intval($pid)));
+        $condition = array('problem_id' => intval($pid));
+        $ret = $collection->findOne($condition);
 
         return $ret;
     }
 
-    public function get_contest_problem($cid, $pid)
-    {
-        $sql = "SELECT
-                    problem.problem_id AS problem_id, problem.title AS title, contest_problem.num AS num, time_limit, memory_limit, submit,
-                    accepted, description, input, output, sample_input, sample_output, hint, source
-                FROM
-                    contest_problem
-                LEFT JOIN
-                    problem ON problem.problem_id = contest_problem.problem_id
-                WHERE
-                    contest_problem.contest_id = {$cid}
-                    AND
-                    contest_problem.num = {$pid}
-        ";
-
-        $result = $this->_db->query(Database::SELECT, $sql, TRUE);
-
-        return $result->current();
-    }
 
     public function get_page($page_id, $per_page = 50)
     {
         $collection = $this->db->selectCollection('problem');
         $start = ($page_id - 1) * $per_page + 1000;
 
-        $cursor =$collection->find(array('problem_id' => array('$gte' => $start)),
-            array('problem_id'=>1, 'title'=>1, 'accepted'=>1, 'submit'=>1))
+        $condition = array('problem_id' => array('$gte' => $start));
+        $need = array('problem_id', 'title', 'accepted', 'submit');
+        $cursor =$collection->find($condition, $this->i_need($need))
             ->sort(array('problem_id' => 1))->limit($per_page);
         //foreach($cursor as $doc) var_dump($doc);
         //var_dump(iterator_to_array($cursor));
@@ -59,245 +42,29 @@ class Model_Problem extends Model_Mongo {
     {
         $collection = $this->db->selectCollection('problem');
 
-        $ret = $collection->count(array('problem_id' => array('$exists' => true)));
+        $condition = array('problem_id' => array('$exists' => true));
+        $ret = $collection->count($condition);
 
         return $ret;
     }
 
     /**
-     * get recent problem information for index page
-     *
+     * @param int $count
      * @return array
+     *
+     * get recent problem information for index page
      */
-    public function get_recent()
+    public function get_recent($count = 5)
     {
-        $key    = 'problem-index';
-        $cache  = Cache::instance();
-        $data   = $cache->get($key);
-        if ($data != null) return $data;
+        $collection = $this->db->selectCollection('problem');
 
-        //fetch data
-        $query = DB::select('problem_id', 'title', 'in_date')
-            ->from('problem')
-            ->limit(5)
-            ->order_by('in_date', 'DESC');
+        $condition = array();
+        $need = array();
 
-        //TODO: fixit
-        $result = $query->as_object()->execute();
-        $cache->set($key, $result, 60);
-        return $result;
-    }
+        $ret = $condition->find($condition, $this->i_need($need))
+            ->sort(array('problem_id' => -1))
+            ->limit($count);
 
-    public function get_status($page_id = 1, $problem_id = null, $user_id = null, $cid = null, $language = null, $result = null)
-    {
-        // fixme: add more set
-        $query = DB::select('solution_id', 'problem_id', 'user_id', 'time', 'memory', 'language', 'result', 'code_length', 'in_date')
-                ->from('solution')
-                ->offset(($page_id - 1) * 20)
-                ->limit(20)
-                ->order_by('solution_id', 'DESC');
-
-        if (!is_null($problem_id) AND !is_null($user_id) AND !is_null($user_id) AND !is_null($cid) AND !is_null($language) AND !is_null($result))
-	    {
-	        $query->where_open();
-	        if (! is_null($problem_id))
-		    {
-		    	$query->where('problem_id', '=', $problem_id);
-		    }
-		    if (! is_null($user_id))
-		    {
-		    	$query->where('user_id', '=', $user_id);
-		    }
-		    if (!is_null($language))
-	        {
-	        	$query->where('language', '=', $language);
-	        }
-	        if (!is_null($result))
-	        {
-	        	$query->where('result', '=', $result);
-	        }
-            if (! is_null($cid))
-            {
-                $query->where('contest_id', '=', $cid);
-            }
-	        $query->where_close();
-    	}
-
-        $result = $query->as_object()->execute();
-
-        $ret = array();
-        foreach($result as $r){
-            $ret[] = $r;
-        }
-
-        return $ret;
-    }
-
-    public function get_summary($pid)
-    {
-        # TODO: add content
-        $key    = "summary-{$pid}";
-        $cache  = Cache::instance();
-        $data   = $cache->get($key);
-        if ($data != null){
-            return $data;
-        }
-
-        $data = array();
-        // get total solutions
-        $sql = "SELECT count(*) AS total FROM solution WHERE problem_id='{$pid}'";
-        $result = $this->_db->query(Database::SELECT, $sql, TRUE);
-        $data['total'] = $result->current()->total;
-
-        // get total user has submited
-        $sql = "SELECT count(DISTINCT user_id) AS total FROM solution WHERE problem_id='{$pid}'";
-        $result = $this->_db->query(Database::SELECT, $sql, TRUE);
-        $data['submit_user'] = $result->current()->total;
-
-        // get total user has ac
-        $sql = "SELECT count(DISTINCT user_id) AS total FROM solution WHERE problem_id='{$pid}'  AND result='4'";
-        $result = $this->_db->query(Database::SELECT, $sql, TRUE);
-        $data['ac_user'] = $result->current()->total;
-
-        // get all status
-        $sql = "SELECT result, count(*) as total FROM solution WHERE problem_id='{$pid}' AND result>=4 GROUP BY result ORDER BY result";
-        $result = $this->_db->query(Database::SELECT, $sql, TRUE);
-
-        $data['more'] = $result->as_array();
-        $cache->set($key, $data, 60);
-        return $data;
-    }
-
-    public function get_best_solution($pid, $start = 0, $limit = 20)
-    {
-        # TODO: add content
-		$key	= "search-$pid-best";
-		$cache	= Cache::instance();
-		$data	= $cache->get($key);
-		if ($data != null) {
-			return $data;
-		}
-		$sql 	= "SELECT solution_id, count(*) att, user_id, language, memory, time, min(10000000000000000000 + time *100000000000 + memory *100000 + code_length) score, in_date
-					FROM solution
-					WHERE result = 4
-					GROUP BY user_id
-					ORDER BY score, in_date
-					LIMIT $start, $limit";
-		$result = $this->_db->query(Database::SELECT, $sql, TRUE);
-
-		$ret = array();
-		foreach($result as $i)
-		{
-			$ret[] = $i;
-		}
-
-        $cache->set($key, $ret, 60);
-
-        return $ret;
-    }
-    
-    public function find_problem($text, $area)
-	{
-		// TODO: add permission
-        $key    = "search-$text-$area";
-        $cache  = Cache::instance();
-        $data   = $cache->get($key);
-        if ($data != null){
-            return $data;
-        }
-		$query = DB::select('problem_id', 'title', 'submit', 'accepted', 'source')
-				->from('problem')
-				->where($area, 'like', "%{$text}%")
-				->order_by('problem_id');
-
-        $result = $query->as_object()->execute();
-
-        $ret = array();
-        foreach($result as $r){
-            $ret[] = $r;
-        }
-        
-        $cache->set($key, $ret, 60);
-        return $ret;
-	}
-	
-	public function get_status_count($problem_id = '', $user_id = '', $language = -1, $result = -1)
-	{   
-        $sql = 'SELECT count(*) AS total FROM solution';
-        
-		$append = '';
-        if (!(($problem_id == '') AND ($user_id == '') AND ($language == -1) AND ($result == -1)))
-	    {
-			$append = ' WHERE (';
-			$flag = FALSE;
-	        if ($problem_id != '')
-		    {
-		    	$append = $append."`problem_id` = '{$problem_id}'";
-		    	$flag = TRUE;
-		    }
-		    if ($user_id != '')
-		    {
-		    	if ($flag) $append = $append. ' AND ';
-		    	$append = $append."`user_id` = '{$user_id}'";
-		    	$flag = TRUE;
-		    }
-		    if ($language != -1)
-	        {
-	        	if ($flag) $append = $append. ' AND ';
-	        	$append = $append."`language` = '{$language}'";
-	        	$flag = TRUE;
-	        }
-	        if ($result != -1)
-	        {
-	        	if ($flag) $append = $append. ' AND ';
-	        	$append = $append."`result` = '{$result}'";
-	        	$flag = TRUE;
-	        }
-	        $append = $append. ')';
-    	}
-    	$sql = $sql. $append;
-
-    	$result = $this->_db->query(Database::SELECT, $sql, TRUE);
-    	$ret = $result->current()->total;
-    	
-    	return $ret;
-	}
-
-    public function new_solution($post)
-    {
-        $sql = DB::select('problem_id')
-                        ->from('contest_problem')
-                        ->where('contest_id', '=', 1000)
-                        ->and_where('num', '=', 0);
-
-        if (isset($post['contest_id']))
-        {
-            //todo: fix get pid from contest
-            $sql = DB::select('problem_id')
-                ->from('contest_problem')
-                ->where('contest_id', '=', $post['contest_id'])
-                ->and_where('num', '=', $post['num']);
-
-            $ret = $sql->execute()->current();
-            $post['problem_id'] = $ret['problem_id'];
-
-            $sql = DB::insert('solution', array('problem_id', 'user_id', 'in_date', 'language', 'ip', 'code_length', 'contest_id', 'num'))
-                ->values(array($post['pid'], $post['user_id'], DB::expr('NOW()'), $post['language'], $post['ip'], strlen($post['source']), $post['cid'], $post['num']));
-        } else {
-            $sql = DB::insert('solution', array('problem_id', 'user_id', 'in_date', 'language', 'ip', 'code_length'))
-                            ->values(array($post['pid'], $post['user_id'], DB::expr('NOW()'), $post['language'], $post['ip'], strlen($post['source'])));
-        }
-
-        list($insert_id, $affect_rows) = $sql->execute();
-
-        //$sql = DB::query(Database::SELECT, "SELECT LAST_INSERT_ID() as last_insert_id FROM test");
-        //$sql = DB::select(array(DB::expr('LAST_INSERT_ID()', 'last_insert_id')))->from('test');
-
-        //TODO: combine source code to solution?
-        $sql = DB::insert('source_code', array('solution_id', 'source'))
-            ->values(array($insert_id, $post['source']));
-
-        $ret = $sql->execute();
-
+        return iterator_to_array($ret);
     }
 }
--- application/views/problem/status.php
+++ application/views/problem/status.php
@@ -13,15 +13,15 @@
 	<tbody>
 <?php foreach($list as $i): ?>
 <tr>
-<td><?php echo $i->solution_id;?></td>
-<td><?php echo html::anchor("/problem/show/{$i->problem_id}", $i->problem_id);?></td>
-<td><?php echo html::anchor("/user/{$i->user_id}", $i->user_id);?></td>
-<td><?php echo OJ::jresult($i->result);?></td>
-<td><?php if($i->result == 4) echo $i->time, 'ms'; else echo '-----'?></td>
-<td><?php if($i->result == 4) echo $i->memory, 'kb'; else echo '-----'?></td>
-<td><?php echo OJ::lang($i->language);?></td>
-<td><?php echo $i->code_length;?>B</td>
-<td><?php echo $i->in_date;?></td>
+<td><?php echo $i['solution_id'];?></td>
+<td><?php echo html::anchor("/problem/show/{$i['problem_id']}", $i['problem_id']);?></td>
+<td><?php echo html::anchor("/user/{$i['user_id']}", $i['user_id']);?></td>
+<td><?php echo OJ::jresult($i['result']);?></td>
+<td><?php if($i['result'] == 4) echo $i['time'], 'ms'; else echo '-----'?></td>
+<td><?php if($i['result'] == 4) echo $i['memory'], 'kb'; else echo '-----'?></td>
+<td><?php echo OJ::lang($i['language']);?></td>
+<td><?php echo $i['code_length'];?>B</td>
+<td><?php echo OJ::mtime($i['add_date']);?></td>
 </tr>
 <?php endforeach;?>
 </tbody>
