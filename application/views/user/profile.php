80f58b0d653d273dc55ffd5eb07c64d7c291d68e
He Jun <freefcw@gmail.com>
2012-02-08 23:11:30 +0800

    improve some ui about user profile

--- application/views/user/profile.php
+++ application/views/user/profile.php
@@ -1,19 +1,33 @@
 <div class="user-profile">
-<h3 class="page-title">About <?php echo $u['user_id']; ?></h3>
-<dl id="basic-info">
-<dt>nick:</dt>
-<dd><?php echo $u['nick'];?></dd>
-<dt>school:</dt>
-<dd><?php echo $u['school'];?></dd>
-<dt>submit:</dt>
-<dd><?php echo $u['submit'];?></dd>
-<dt>solved:</dt>
-<dd><?php echo $u['solved'];?></dd>
-<dt>email:</dt>
-<dd><?php echo $u['email'];?></dd>
-<dt>last access time:</dt>
-<dd><?php echo OJ::mtime($u['access_time']);?></dd>
-<dt>reg time:</dt>
-<dd><?php echo OJ::mtime($u['reg_time']);?></dd>
-</dl>
+<div class="row">
+    <div class="span1">
+        <!--<img src="#userimage" alt="user image"/>-->
+    </div>
+    <div class="span8">
+        <h2><?php echo $u['user_id'];?></h2>
+        <p ><?php echo $u['nick'];?></p>
+        <p style="color: rgba(0, 0, 0, 0.15);"><?php echo $u['user_id'];?> 出生于公元<?php echo OJ::mtime($u['reg_time']);?>, 最后一次访问本站的时间是 <?php echo OJ::mtime($u['access_time']);?></p>
+       <ul class="nav nav-list">
+           <li><i class="icon-home"></i><?php echo $u['school'];?></li>
+           <li><i class="icon-inbox"></i><?php echo $u['submit'];?> submission</li>
+           <li><i class="icon-ok-sign"></i><?php echo $u['solved'];?> accepted</li>
+       </ul>
+    </div>
+</div>
+<div class="row">
+    <div class="span10">
+    <h3>Recent Topic</h3>
+    <ul class="nav">
+        <li></li>
+    </ul>
+    </div>
+</div>
+<div class="row">
+    <div class="span10">
+    <h3>Recent Passed Problem</h3>
+    <ul class="nav">
+        <li></li>
+    </ul>
+    </div>
+</div>
 </div>
