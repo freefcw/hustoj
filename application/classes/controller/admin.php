00d8328fe18007027c05488e00edb31761894550
He Jun <freefcw@gmail.com>
2012-02-09 03:01:13 +0800

    add admin controller

---
+++ application/classes/controller/admin.php
@@ -0,0 +1,25 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Admin extends Controller_My{
+
+    public function before()
+    {
+        parent::before();
+        if (!Auth::instance()->is_admin()) $this->request->redirect('/');
+    }
+
+    public function action_index()
+    {
+        $this->view->title = 'Admin Control Panel';
+        $this->view->body = 'hi, admin';
+    }
+
+    public function action_problemlist()
+    {
+        $this->view->title = 'Problem List';
+        $this->view->body = 'this is problem list';
+    }
+
+    public function action_userlist()
+    {}
+}
