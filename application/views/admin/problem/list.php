0d2a34c251ab00faafec2837651ce6c0703a6d54
He Jun <freefcw@gmail.com>
2012-03-04 03:23:27 +0800

    add edit link

--- application/views/admin/problem/list.php
+++ application/views/admin/problem/list.php
@@ -5,7 +5,7 @@
 <?php foreach($problem as $p):?>
 <tr>
 <td><?php echo $p['problem_id'];?></td>
-<td><?php echo $p['title'];?><a href="/admin/edit/<?php echo $p['problem_id'];?>" style="float: right;">[EDIT]</a></td>
+<td><?php echo $p['title'];?><a href="/admin/problem/edit/<?php echo $p['problem_id'];?>" style="float: right;">[EDIT]</a></td>
 <td><?php echo OJ::mtime($p['add_date']);?></td>
 <td>Public</td>
 <td><a id="del-<?php echo $p['problem_id'];?>">Del</a></td>
