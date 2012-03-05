d4589267dc81a1c355d4c92dc058100f0ccc5ddb
He Jun <freefcw@gmail.com>
2012-03-05 22:30:17 +0800

    save user information

--- application/classes/controller/admin/user.php
+++ application/classes/controller/admin/user.php
@@ -24,10 +24,31 @@ class Controller_Admin_User extends Controller_Admin_My{
     public function action_edit()
     {
         $user_id = $this->request->param('id', null);
-
         if ($user_id === null) $this->error_page();
-
         $m = new Model_User();
+        if ($this->request->method() == 'POST')
+        {
+            $post = $this->request->post();
+
+            $newdata = array(
+                'disable'=> false,
+            );
+            $newdata['user_id'] = $post['user_id'];
+            $user_id = $post['user_id'];
+            $newdata['school'] = $post['school'];
+            $newdata['intro'] = $post['intro'];
+            $newdata['email'] = $post['email'];
+
+            if ($post['disabled'] == 1)
+                $newdata['disabled'] = true;
+
+            if ($post['password'] AND $post['repassword'] == $post['password'])
+            {
+                $newdata['password'] = Auth::instance()->hash($post['password']);
+            }
+
+            $ret = $m->update_information($newdata);
+        }
         $u = $m->get_info_by_name($user_id);
 
         $body = View::factory('/admin/user/edit');
