800c281e275432d5601a520e2f603d9d7e6826ae
He Jun <freefcw@gmail.com>
2012-03-09 23:02:59 +0800

    what's happened? replaced my file with another file!!!

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,17 +1,195 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
+<?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Admin_Setting extends Controller_Admin_My{
+// -- Environment setup --------------------------------------------------------
 
-    public function action_index()
-    {
-        $body = View::factory('admin/setting/index');
+// Load the core Kohana class
+require SYSPATH.'classes/kohana/core'.EXT;
 
-        $this->view->title = 'General Setting';
-        $this->view->body = $body;
-    }
-
-    public function action_system(){}
-
-    public function action_discuss(){}
-
+if (is_file(APPPATH.'classes/kohana'.EXT))
+{
+	// Application extends the core
+	require APPPATH.'classes/kohana'.EXT;
 }
+else
+{
+	// Load empty core extension
+	require SYSPATH.'classes/kohana'.EXT;
+}
+
+/**
+ * Set the default time zone.
+ *
+ * @see  http://kohanaframework.org/guide/using.configuration
+ * @see  http://php.net/timezones
+ */
+date_default_timezone_set('Asia/Shanghai');
+
+/**
+ * Set the default locale.
+ *
+ * @see  http://kohanaframework.org/guide/using.configuration
+ * @see  http://php.net/setlocale
+ */
+setlocale(LC_ALL, 'zh_CN.utf-8');
+
+/**
+ * Enable the Kohana auto-loader.
+ *
+ * @see  http://kohanaframework.org/guide/using.autoloading
+ * @see  http://php.net/spl_autoload_register
+ */
+spl_autoload_register(array('Kohana', 'auto_load'));
+
+/**
+ * Enable the Kohana auto-loader for unserialization.
+ *
+ * @see  http://php.net/spl_autoload_call
+ * @see  http://php.net/manual/var.configuration.php#unserialize-callback-func
+ */
+ini_set('unserialize_callback_func', 'spl_autoload_call');
+
+// -- Configuration and initialization -----------------------------------------
+
+/**
+ * Set the default language
+ */
+I18n::lang('zh_CN');
+
+/**
+ * Set Kohana::$environment if a 'KOHANA_ENV' environment variable has been supplied.
+ *
+ * Note: If you supply an invalid environment name, a PHP warning will be thrown
+ * saying "Couldn't find constant Kohana::<INVALID_ENV_NAME>"
+ */
+if (isset($_SERVER['KOHANA_ENV']))
+{
+	Kohana::$environment = constant('Kohana::'.strtoupper($_SERVER['KOHANA_ENV']));
+}
+
+/**
+ * Initialize Kohana, setting the default options.
+ *
+ * The following options are available:
+ *
+ * - string   base_url    path, and optionally domain, of your application   NULL
+ * - string   index_file  name of your index file, usually "index.php"       index.php
+ * - string   charset     internal character set used for input and output   utf-8
+ * - string   cache_dir   set the internal cache directory                   APPPATH/cache
+ * - boolean  errors      enable or disable error handling                   TRUE
+ * - boolean  profile     enable or disable internal profiling               TRUE
+ * - boolean  caching     enable or disable internal caching                 FALSE
+ */
+Kohana::init(array(
+	'base_url'   => '/',
+	'index_file' => '/',
+));
+
+/**
+ * Attach the file write to logging. Multiple writers are supported.
+ */
+Kohana::$log->attach(new Log_File(APPPATH.'logs'));
+
+/**
+ * Attach a file reader to config. Multiple readers are supported.
+ */
+Kohana::$config->attach(new Config_File);
+
+/**
+ * Enable modules. Modules are referenced by a relative or absolute path.
+ */
+Kohana::modules(array(
+	 'auth'       => MODPATH.'auth',       // Basic authentication
+	 'cache'      => MODPATH.'cache',      // Caching with multiple backends
+	// 'codebench'  => MODPATH.'codebench',  // Benchmarking tool
+	'database'   => MODPATH.'database',   // Database access
+	// 'image'      => MODPATH.'image',      // Image manipulation
+	// 'orm'        => MODPATH.'orm',        // Object Relationship Mapping
+	// 'unittest'   => MODPATH.'unittest',   // Unit testing
+	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
+	));
+
+Cookie::$salt = 'hustoj';
+
+/**
+ * Set the routes. Each route must have a minimum of a name, a URI and a set of
+ * defaults for the URI.
+ */
+
+Route::set('auth', '<action>',
+	array('action' => '(login|logout|setting|setting)'))
+	->defaults(array(
+		'controller' => 'user'
+	));
+Route::set('search', '/problem/search')
+	->defaults(array(
+		'controller' => 'problem',
+		'action' => 'search'
+	));
+
+Route::set('ranklist', 'rank/user(/<id>)')
+	->defaults(array(
+		'controller' => 'user',
+		'action'	 => 'list'
+	));
+Route::set('user', 'user/<id>',
+	array('id' => '[[:word:]]+'))
+	->defaults(array(
+		'controller' => 'user',
+		'action' => 'profile'
+	));
+
+Route::set('page', '<action>',
+	array(
+		'action' => '(home|faqs|about|links|contact|status|help|terms)'
+	))
+	->defaults(array(
+		'controller' => 'index'
+	));
+Route::set('contest-problem', 'contest/<cid>/problem/<pid>',
+    array('cid' => '\d+',
+        'pid' => '\d+'
+    ))->defaults(array(
+        'controller'    => 'contest',
+        'action'        => 'problem',
+    ));
+Route::set('adminindex', 'admin')
+	->defaults(array(
+		'controller' => 'admin_index',
+        'action'     => 'index'
+	)
+);
+Route::set('adminuser', 'admin/user(/<action>(/<id>))')
+	->defaults(array(
+		'controller' => 'admin_user',
+        'action'     => 'index'
+	)
+);
+Route::set('adminproblem', 'admin/problem(/<action>(/<id>))')
+	->defaults(array(
+		'controller' => 'admin_problem',
+        'action'     => 'index'
+	)
+);
+Route::set('admincontest', 'admin/contest(/<action>(/<id>))')
+	->defaults(array(
+		'controller' => 'admin_contest',
+        'action'     => 'index'
+	)
+);
+
+Route::set('adminsetting', 'admin/setting(/<action>(/<id>))')
+	->defaults(array(
+		'controller' => 'admin_setting',
+        'action'     => 'index'
+	)
+);
+
+Route::set('default', '(<controller>(/<action>(/<id>(/<overflow>))))',
+	array(
+		'id' => '[[:digit:]]{1,}',
+		'overflow' => '.*?'
+	))
+	->defaults(array(
+		'controller' => 'index',
+		'action'     => 'index',
+	));
