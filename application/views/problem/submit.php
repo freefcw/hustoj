4d7d84a744d3da8c3b82d0f7ac091372890564d6
He Jun <freefcw@gmail.com>
2012-05-13 22:10:44 +0800

    fix show user not login when submit solution

--- application/views/problem/submit.php
+++ application/views/problem/submit.php
@@ -19,14 +19,22 @@
 <?php echo Form::label('pid', "Language : ");?>
 <div class="controls">
 <?php echo Form::select('language', array('0'=>'C', '1'=>'C++', '2'=>'Pascal', '3'=>'Java'));?>
+</div>
+</div>
 <?php if (Auth::instance()->get_user() == null):?>
-    <?php echo Form::label('user_id', "Username : ");?>
-    <?php echo Form::input('user_id', Cookie::get('use_id', '')); ?>
-    <?php echo Form::label('password', "Password : ");?>
-    <?php echo Form::input('password'); ?>
+    <div class="control-group">
+    <?php echo Form::label('user_id', "Username : ", array('class', 'control-label'));?>
+    <div class="controls">
+        <?php echo Form::input('user_id', Cookie::get('use_id', '')); ?>
+    </div>
+    </div>
+    <div class="control-group">
+    <?php echo Form::label('password', "Password : ", array('class', 'control-label'));?>
+    <div class="controls">
+        <?php echo Form::password('password'); ?>
+    </div>
+    </div>
 <?php endif;?>
-</div>
-</div>
 </fieldset>
 <?php echo Form::textarea('source');?>
 <div class="form-actions">
