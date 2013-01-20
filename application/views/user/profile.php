6e187c13ef5b30c16eea2824f0ee060e09f587bb
freefcw <freefcw@gmail.com>
2013-01-20 16:53:40 +0800

    rafactor user profile

--- application/views/user/profile.php
+++ application/views/user/profile.php
@@ -1,33 +1,25 @@
 <div class="user-profile">
-<div class="row">
-    <div class="span1">
-        <!--<img src="#userimage" alt="user image"/>-->
-    </div>
-    <div class="span8">
-        <h2><?php echo $u['user_id'];?></h2>
-        <p ><?php echo $u['nick'];?></p>
-        <p style="color: rgba(0, 0, 0, 0.15);"><?php echo $u['user_id'];?> 出生于公元<?php echo OJ::mtime($u['reg_time']);?>, 最后一次访问本站的时间是 <?php echo OJ::mtime($u['access_time']);?></p>
-       <ul class="nav nav-list">
-           <li><i class="icon-home"></i><?php echo $u['school'];?></li>
-           <li><i class="icon-inbox"></i><?php echo $u['submit'];?> submission</li>
-           <li><i class="icon-ok-sign"></i><?php echo $u['solved'];?> accepted</li>
-       </ul>
-    </div>
+ <div class="span8">
+     <!--<img src="#userimage" alt="user image"/>-->
+    <h2><?php echo $u['user_id'];?></h2>
+    <p ><?php echo $u['nick'];?></p>
 </div>
 <div class="row">
-    <div class="span10">
-    <h3>Recent Topic</h3>
-    <ul class="nav">
-        <li></li>
-    </ul>
+    <div class="span6 user-card">
+        <ul class="nav nav-pills nav-stacked">
+           <li><a><i class="icon-signal"></i> 5</a></li>
+           <li><a><i class="icon-home"></i><?php echo $u['school'];?></a></li>
+           <li><a><i class="icon-envelope"></i> <?php echo $u['email'];?></a></li>
+           <li><a><i class="icon-time"></i>出生于公元 <?php echo OJ::getdate($u['reg_time']);?></a></li>
+           <li><a><i class="icon-time"></i>上次访问时间 <?php echo OJ::getdate($u['access_time']);?></a></li>
+        </ul>
+    </div>
+    <div class="span4 user-stats">
+        <ul class="nav nav-pills">
+            <li class="stats active"><a><span class="count"><?php echo $u['submit'];?></span>submission</a> </li>
+            <li class="stats active"><a><span class="count"><?php echo $u['solved'];?></span>accepted</a> </li>
+        </ul>
     </div>
 </div>
-<div class="row">
-    <div class="span10">
-    <h3>Recent Passed Problem</h3>
-    <ul class="nav">
-        <li></li>
-    </ul>
-    </div>
-</div>
+
 </div>
