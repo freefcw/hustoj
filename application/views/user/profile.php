4573a62864f4029dc069f51f0121a2917e80c188
freefcw <freefcw@gmail.com>
2013-01-22 00:33:52 +0800

    update user profile style

--- application/views/user/profile.php
+++ application/views/user/profile.php
@@ -5,19 +5,19 @@
     <p ><?php echo $u['nick'];?></p>
 </div>
 <div class="row">
-    <div class="span6 user-card">
+    <div class="span8 user-card">
         <ul class="nav nav-pills nav-stacked">
            <li><a><i class="icon-signal"></i> 5</a></li>
-           <li><a><i class="icon-home"></i><?php echo $u['school'];?></a></li>
+           <li><a><i class="icon-home"></i> <?php echo $u['school'];?></a></li>
            <li><a><i class="icon-envelope"></i> <?php echo $u['email'];?></a></li>
-           <li><a><i class="icon-time"></i>出生于公元 <?php echo OJ::getdate($u['reg_time']);?></a></li>
-           <li><a><i class="icon-time"></i>上次访问时间 <?php echo OJ::getdate($u['access_time']);?></a></li>
+           <li><a><i class="icon-time"></i> 出生于公元 <?php echo OJ::getdate($u['reg_time']);?></a></li>
+           <li><a><i class="icon-time"></i> 上次访问时间 <?php echo OJ::getdate($u['access_time']);?></a></li>
         </ul>
     </div>
-    <div class="span4 user-stats">
+    <div class="span3 user-stats">
         <ul class="nav nav-pills">
-            <li class="stats active"><a><span class="count"><?php echo $u['submit'];?></span>submission</a> </li>
-            <li class="stats active"><a><span class="count"><?php echo $u['solved'];?></span>accepted</a> </li>
+            <li class="stats active count-accept"><a><span class="count"><?php echo $u['solved'];?></span>accepted</a> </li>
+            <li class="stats active count-submit"><a><span class="count"><?php echo $u['submit'];?></span>submission</a> </li>
         </ul>
     </div>
 </div>
