eb38932c72eaa53cf0953ccffcdb7f05a4c17b9a
freefcw <freefcw@gmail.com>
2013-01-25 00:20:44 +0800

    phpdoc

--- application/classes/model/problem.php
+++ application/classes/model/problem.php
@@ -5,16 +5,25 @@
  * @author freefcw
  */
 
-class Model_Problem extends Model_Mongo {
+class Model_Problem extends Model_Mongo
+{
 
     var $collection;
 
+    /**
+     *
+     */
     public function __construct()
     {
         parent::__construct();
         $this->collection = $this->db->selectCollection('problem');
     }
 
+    /**
+     * @param $pid
+     *
+     * @return array|null
+     */
     public function get_problem($pid)
     {
         $condition = array('problem_id' => intval($pid));
@@ -23,6 +32,11 @@ class Model_Problem extends Model_Mongo {
         return $ret;
     }
 
+    /**
+     * @param $id
+     *
+     * @return array|null
+     */
     public function get_problem_by_id($id)
     {
         $condition = array('_id' => $id);
@@ -32,38 +46,52 @@ class Model_Problem extends Model_Mongo {
 
     }
 
+    /**
+     * @param     $page_id
+     * @param int $per_page
+     *
+     * @return array
+     */
     public function get_page($page_id, $per_page = 50)
     {
         $start = ($page_id - 1) * $per_page + 1000;
 
         $condition = array('problem_id' => array('$gte' => $start));
         $need = array('problem_id', 'title', 'accepted', 'submit');
-        $cursor =$this->collection->find($condition, $this->i_need($need))
+        $cursor = $this->collection->find($condition, $this->i_need($need))
             ->sort(array('problem_id' => 1))->limit($per_page);
         //foreach($cursor as $doc) var_dump($doc);
         //var_dump(iterator_to_array($cursor));
 
         return iterator_to_array($cursor);
     }
+
+    /**
+     * @param     $page_id
+     * @param int $per_page
+     *
+     * @return array
+     */
     public function get_page_for_admin($page_id, $per_page = 50)
     {
         $start = ($page_id - 1) * $per_page + 1000;
 
         $condition = array('problem_id' => array('$gte' => $start));
         $need = array('problem_id', 'title', 'deleted', 'add_date');
-        $cursor =$this->collection->find($condition, $this->i_need($need))
+        $cursor = $this->collection->find($condition, $this->i_need($need))
             ->sort(array('problem_id' => 1))->limit($per_page);
         //foreach($cursor as $doc) var_dump($doc);
         //var_dump(iterator_to_array($cursor));
 
         return iterator_to_array($cursor);
     }
+
     /**
-    * return total problems
-    *
-    *  @author freefcw
-    *  @return int
-    */
+     * return total problems
+     *
+     * @author freefcw
+     * @return int
+     */
     public function get_total()
     {
         $condition = array('problem_id' => array('$exists' => true));
@@ -74,6 +102,7 @@ class Model_Problem extends Model_Mongo {
 
     /**
      * @param int $count
+     *
      * @return array
      *
      * get recent problem information for index page
@@ -90,7 +119,7 @@ class Model_Problem extends Model_Mongo {
         return iterator_to_array($ret);
     }
 
-    public function find_problem($text, $area, $orderby='problem_id')
+    public function find_problem($text, $area, $orderby = 'problem_id')
     {
         // $erea can be title|content
         $regexObject = new MongoRegex("/{$text}/i");
@@ -117,8 +146,7 @@ class Model_Problem extends Model_Mongo {
 
     public function save($post, $is_contest = false)
     {
-        if (array_key_exists('problem_id', $post))
-        {
+        if (array_key_exists('problem_id', $post)) {
             $condition = array('problem_id' => $post['problem_id']);
             $ret = $this->collection->update($condition, array('$set' => $post));
         } else {
