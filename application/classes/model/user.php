8e4400fe796902e7945790674c0d08fe228b7a9a
freefcw <freefcw@gmail.com>
2013-01-24 18:59:36 +0800

    add basic phpdoc

--- application/classes/model/user.php
+++ application/classes/model/user.php
@@ -4,15 +4,25 @@
  *
  * @author freefcw
  */
-class Model_User extends Model_Mongo {
-    
+class Model_User extends Model_Mongo
+{
+
+    /**
+     *
+     */
     public function __construct()
     {
         parent::__construct();
         //will load database library into $this->db, you can leave it out if you don't need it
         $this->collection = $this->db->selectCollection('user');
     }
-    
+
+    /**
+     * @param $username
+     * @param $password
+     *
+     * @return bool
+     */
     public function auth($username, $password)
     {
         $condition = array();
@@ -24,41 +34,56 @@ class Model_User extends Model_Mongo {
 
         $this->log_auth($username, $password);
 
-        if ($num == 0 ) return false;
+        if ($num == 0) {
+            return false;
+        }
         // if user login success, then log last access time
         $this->update_access_time($username);
         $u = $this->collection->findOne($condition);
-        if (array_key_exists('privilege', $u))
+        if (array_key_exists('privilege', $u)) {
             Session::instance()->set('privilege', $u['privilege']);
-        else
-            // default permission is insert on user create or here? new user should has the default permission...
+        } else // default permission is insert on user create or here? new user should has the default permission...
+        {
             Session::instance()->set('privilege', 'user');
+        }
 
         return true;
     }
 
+    /**
+     * @param $user_id
+     * @param $pwd
+     */
     private function log_auth($user_id, $pwd)
     {
         $log = array(
-            'type' => 'login',
-            'user_id' => $user_id,
+            'type'     => 'login',
+            'user_id'  => $user_id,
             'password' => Auth_Hoj::instance()->hash($pwd),
-            'ip' => Request::$client_ip,
-            'time' => new MongoDate(time()),
+            'ip'       => Request::$client_ip,
+            'time'     => new MongoDate(time()),
         );
 
         $log_collection = $this->db->selectCollection('logs');
         $log_collection->save($log);
     }
 
+    /**
+     * @param $user_id
+     */
     private function update_access_time($user_id)
     {
         $new_value = array('access_time' => new MongoDate(time()));
 
         $collection = $this->db->selectCollection('user');
-        $collection->update(array('user_id'=>$user_id), array('$set'=>$new_value));
+        $collection->update(array('user_id' => $user_id), array('$set' => $new_value));
     }
 
+    /**
+     * @param $username
+     *
+     * @return mixed
+     */
     public function get_password($username)
     {
         $need = array('password');
@@ -70,31 +95,43 @@ class Model_User extends Model_Mongo {
 
         return $ret['password'];
     }
+
     /**
-         *
-         * @param <int> $user_id
-         * @param <string> $new_password
-         */
+     *
+     * @param <int> $user_id
+     * @param <string> $new_password
+     */
     public function changepassword($user_id, $new_password)
     {
-        $new_value = array('password'=>$new_password);
+        $new_value = array('password' => $new_password);
 
-        $this->collection->update(array('user_id'=>$user_id), array('$set'=>$new_value));
+        $this->collection->update(array('user_id' => $user_id), array('$set' => $new_value));
     }
+
     /**
-         * 通过用户名返回用户信息
-         *
-         * @param <type> $user_id
-         * @return <mixed> user imformation
-         */
+     * 通过用户名返回用户信息
+     *
+     * @param $user_id
+     *
+     * @return array|null <mixed> user imformation
+     */
     public function get_info_by_name($user_id)
     {
-        $ret = $this->collection->findOne(array('user_id'=>$user_id));
+        $ret = $this->collection->findOne(array('user_id' => $user_id));
 
         return $ret;
     }
 
 
+    /**
+     *
+     * get user list
+     *
+     * @param     $page_id
+     * @param int $per_page
+     *
+     * @return array
+     */
     public function get_list($page_id, $per_page = 50)
     {
         $need = array('user_id', 'nick', 'solved', 'submit');
@@ -103,13 +140,18 @@ class Model_User extends Model_Mongo {
 
         $ret = $this->collection->find($condition, $this->i_need($need))
             ->sort(array('solved' => -1))
-            ->skip(($page_id-1) * $per_page)
+            ->skip(($page_id - 1) * $per_page)
             ->limit($per_page);
         //TODO: skip performance
 
         return iterator_to_array($ret);
     }
 
+    /**
+     * get total user
+     *
+     * @return int
+     */
     public function get_total()
     {
         // TODO: more params
@@ -118,37 +160,54 @@ class Model_User extends Model_Mongo {
         return $ret;
     }
 
+    /**
+     * if user exist
+     *
+     * @param $user_id
+     *
+     * @return bool
+     */
     public function exist_id($user_id)
     {
-        $condition = array('user_id'=>$user_id);
+        $condition = array('user_id' => $user_id);
         $ret = $this->collection->count($condition);
 
-        if($ret != 0) return true;
+        if ($ret != 0) {
+            return true;
+        }
         return false;
     }
 
+    /**
+     * @param $user
+     *
+     * @return bool
+     */
     public function save($user)
     {
-        $condition = array('user_id'=>$user['user_id']);
-        $ret = $this->collection->update($condition, array('$set'=>$user));
+        $condition = array('user_id' => $user['user_id']);
+        $ret = $this->collection->update($condition, array('$set' => $user));
 
         return $ret;
     }
 
+    /**
+     * @param $user
+     */
     public function add_user($user)
     {
         $now = new MongoDate(time());
         $newuser = array(
-            'password' =>Auth::instance()->hash($user['password']),
-            'user_id' => $user['username'],
-            'school' => $user['school'],
-            'email' => $user['email'],
-            'nick' => $user['nick'],
-            'reg_time' => $now,
-            'solved' => 0,
-            'submit' => 0,
+            'password'    => Auth::instance()->hash($user['password']),
+            'user_id'     => $user['username'],
+            'school'      => $user['school'],
+            'email'       => $user['email'],
+            'nick'        => $user['nick'],
+            'reg_time'    => $now,
+            'solved'      => 0,
+            'submit'      => 0,
             'access_time' => $now,
-            'ip' => Request::$client_ip,
+            'ip'          => Request::$client_ip,
         );
         //FIXME: seems ip and access_time no use
 
@@ -163,8 +222,8 @@ class Model_User extends Model_Mongo {
     public function ban($uid)
     {
         $condition = array('user_id' => $uid);
-        $data= array('disable' => true);
+        $data = array('disable' => true);
 
-        $this->collection->update($condition, array('$set'=> $data));
+        $this->collection->update($condition, array('$set' => $data));
     }
 }
