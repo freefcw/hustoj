1b60b7034c2d20bfdbe738886ea368caa6f5ecb1
freefcw <freefcw@gmail.com>
2013-01-30 23:24:13 +0800

    add topic show

--- application/views/discuss/topic.php
+++ application/views/discuss/topic.php
@@ -1,4 +1,10 @@
 <div class="relies">
+    <h1><?php echo $the_topic['title'];?></h1>
+    <strong><a href="/user/<?php echo $the_topic['user_id'];?>"><?php echo $the_topic['user_id'];?></a></strong> post
+    at <?php echo OJ::mtime($the_topic['date']);?>
+    <div>
+        <?php echo nl2br($the_topic['content']);?>
+    </div>
     <?php foreach ($relies as $r): ?>
     <div class="reply" id="reply-<?php echo $r['reply_id'];?>">
         <a href="/user/<?php echo $r['user_id'];?>"><?php echo $r['user_id'];?></a>
