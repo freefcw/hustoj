649bf73cbd2191ce49c6926fd78708bb3848889d
freefcw <freefcw@gmail.com>
2013-02-01 00:18:48 +0800

    add more config for error handle

--- application/classes/controller/my.php
+++ application/classes/controller/my.php
@@ -1,30 +1,33 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_My extends Controller {
+class Controller_My extends Controller
+{
 
-	protected $view;
+    protected $view;
 
-	public function before()
-	{
-		$this->view = View::factory('layout');
+    public function before()
+    {
+        $this->view = View::factory('layout');
 
         // default is null
 
         $this->view->current_user = Auth::instance()->get_user();
 
-		# TODO: add admin control
+        # TODO: add admin control
 
-	}
+    }
 
-    public function error_page($msg = '')
+    public function error_page($msg = '', $page = 'error')
     {
-        $body = View::factory('admin/index/err_page');
+        $body = View::factory("admin/index/{$page}");
+        $body->msg = $msg;
+
         $this->view->body = $body;
     }
 
-	public function after()
-	{
-		$this->response->body($this->view);
-	}
+    public function after()
+    {
+        $this->response->body($this->view);
+    }
 
 } // End Welcome
