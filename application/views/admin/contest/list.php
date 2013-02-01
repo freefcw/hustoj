20d7de05d70b500e6371b2e91026d4b4d50cf7db
freefcw <freefcw@gmail.com>
2013-02-02 01:23:26 +0800

    add edit member link for private contest

--- application/views/admin/contest/list.php
+++ application/views/admin/contest/list.php
@@ -9,6 +9,7 @@
 <td><?php echo html::anchor("/contest/show/{$c['contest_id']}", $c['title']);?></td>
 <td><?php echo OJ::mtime($c['start_time']);?> <br/> <?php echo OJ::mtime($c['end_time']);?></td>
 <td class="capitalize"><a href="/admin/contest/changeprivate/<?php echo OJ::private_value($c['private']);?>"><?php echo OJ::private_value($c['private']);?></a>
+    <?php if (OJ::is_private($c['private'])):?>| <a href="/admin/contest/listuser/<?php echo $c['contest_id'];?>">Edit Member</a><?php endif;?>
 </td>
 </tr>
 <?php endforeach; ?>
