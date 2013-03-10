e41af26eeb185d5183ed609b95983a45ece9e59b
freefcw <freefcw@gmail.com>
2013-03-10 17:48:38 +0800

    add breadcrumb

--- application/views/discuss/topic.php
+++ application/views/discuss/topic.php
@@ -1,19 +1,24 @@
-<h1><?php echo $the_topic['title'];?></h1>
-<strong><a href="/user/<?php echo $the_topic['user_id'];?>"><?php echo $the_topic['user_id'];?></a></strong> post
-at <?php echo OJ::mtime($the_topic['date']);?>
-<div>
+<ul class="breadcrumb">
+    <li><a href="/discuss/">Discuss</a> <span class="divider">/</span></li>
+    <li><a href="/discuss?pid=<?php echo $the_topic['pid'];?>"><?php echo $the_topic['pid'];?></a> <span class="divider">/</span></li>
+</ul>
+<h2><?php echo $the_topic['title'];?></h2>
+<a href="/user/<?php echo $the_topic['user_id'];?>"><?php echo $the_topic['user_id'];?></a>
+ post at <?php echo OJ::mtime($the_topic['date']);?>
+<div class="readability">
     <?php echo nl2br($the_topic['content']);?>
 </div>
 <hr />
+
 <?php foreach ($relies as $r): ?>
 <div class="reply" id="reply-<?php echo $r['reply_id'];?>">
-    <a href="/user/<?php echo $r['user_id'];?>"><?php echo $r['user_id'];?></a>
-
+    <a href="/user/<?php echo $r['user_id'];?>"><?php echo $r['user_id'];?></a> reply at <?php echo OJ::timesince($r['date']);?>
     <p>
         <?php echo $r['content'];?>
     </p>
 </div>
 <?php endforeach;?>
+
 <?php if (Auth::instance()->get_user()):?>
 <form action="/discuss/topic/<?php echo $the_topic['topic_id'];?>" method="POST" class="form-horizontal">
     <fieldset>
