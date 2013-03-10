2767c7836ea2de823a6d00b92ca4a1baf3997a89
freefcw <freefcw@gmail.com>
2013-03-10 17:47:39 +0800

    change some ui for topic list

--- application/views/discuss/list.php
+++ application/views/discuss/list.php
@@ -21,8 +21,7 @@
         <td>
             <a href="/problem/show/<?php echo $t['pid'];?>" style="color: #000000"> <?php echo $t['pid'];?> </a>
 
-            <h3 style="display: inline-block"><a href="/t/<?php echo $t['topic_id'];?>"><?php echo $t['title'];?></a>
-            </h3>
+            <a href="/t/<?php echo $t['topic_id'];?>"><strong><?php echo $t['title'];?></strong></a>
         </td>
         <td><a href="/user/<?php echo $t['user_id'];?>"><?php echo $t['user_id'];?></a></td>
         <td><?php echo OJ::mtime($t['last_reply']);?></td>
