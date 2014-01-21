0cd50bd7dc0df8eb74c4374b2f00f00436adbbd9
He Jun <freefcw@gmail.com>
2014-01-21 21:02:40 +0800

    rewrite ga code, make it useable
    refer: ruby-china

--- application/views/block/ga.php
+++ application/views/block/ga.php
@@ -1,14 +1,18 @@
-<?php $ga_code = Model_Option::get_option('ga_code');
-if (isset($ga_code)):?>
-<script type="text/javascript" data-turbolinks-eval="false">
-    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript" data-turbolinks-eval="false">
-    try {
-        var ga_code  = '<?php echo($ga_code);?>';
-        var pageTracker = _gat._getTracker(ga_code);
-        pageTracker._trackPageview();
-    } catch(err) {}
+<?php $gacode = Model_Option::get_option('ga_code');
+if ( isset($gacode) ):?>
+<script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', '<?php echo($gacode);?>']);
+    _gaq.push(['_setSiteSpeedSampleRate', 100]);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+        if ( $('script[id=gajs]').length == 0 )
+        {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.id = 'gascript';
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        }
+    })();
 </script>
 <?php endif;?>
