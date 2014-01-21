ecaef5619da81b14f40f9c551831e02e081f5ccb
He Jun <freefcw@gmail.com>
2014-01-21 20:06:50 +0800

    修改footer的时间

--- application/views/footer.php
+++ application/views/footer.php
@@ -8,6 +8,6 @@
         <li><?php echo(HTML::anchor('terms', 'Terms of Service'));?></li>
     </ul>
     <div class="runtime-info">{memory_usage}, {execution_time}</div>
-    <p id="copyright">Copyright © 2003-2011 <?php echo(HTML::anchor('http://acm.hust.edu.cn', 'HUST ACMICPC TEAM'));?>. All rights reserved.</p>
+    <p id="copyright">Copyright © 2003 - <?php echo(date('Y'));?> <?php echo(HTML::anchor('http://acm.hust.edu.cn', 'HUST ACMICPC TEAM'));?>. All rights reserved.</p>
 </div>
 <?php echo View::factory('block/ga');?>
