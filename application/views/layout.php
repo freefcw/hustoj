50aca4a8340796b2585dbe7b96fbe6777db27431
He Jun <freefcw@gmail.com>
2014-01-21 21:56:14 +0800

    add nprogress.js

--- application/views/layout.php
+++ application/views/layout.php
@@ -8,7 +8,7 @@
     <title><?php echo($title); ?></title>
     <link rel="stylesheet" href="/css/bootstrap.css">
     <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="/css/nprogress.css">
     <link rel="stylesheet" href="/js/code/prettify.css">
     <link type="text/css" href="/css/ui-lightness/jquery-ui-1.10.3.custom.min.css" rel="stylesheet" />
     <script type="text/javascript" src="/js/jquery-1.9.1.js"></script>
@@ -18,6 +18,7 @@
     <script type="text/javascript" src="/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="/js/jquery-ui-1.10.3.custom.js"></script>
     <script type="text/javascript" src="/js/turbolinks.js"></script>
+    <script type="text/javascript" src="/js/nprogress.js"></script>
     <link rel="shortcut icon" href="/favicon.ico"/>
 </head>
 <body>
--- js/page.js
+++ js/page.js
@@ -3,3 +3,9 @@ $(function(){
         return confirm('ARE U SURE???')
     })
 });
+
+$(function(){
+    $(document).on('page:fetch',   function() { NProgress.start(); });
+    $(document).on('page:change',  function() { NProgress.done(); });
+    $(document).on('page:restore', function() { NProgress.remove(); });
+});
