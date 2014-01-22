52bb8411889170f19d3894775cff4803c304fb1b
He Jun <freefcw@gmail.com>
2014-01-22 23:43:58 +0800

    移除无用代码

--- application/views/admin/layout.php
+++ application/views/admin/layout.php
@@ -20,12 +20,6 @@
 <div class="navbar navbar-inverse" role="navigation">
     <div class="container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
             <a class="navbar-brand" href="/admin">ADMIN</a>
         </div>
         <div class="collapse navbar-collapse">
--- application/views/layout.php
+++ application/views/layout.php
@@ -25,12 +25,6 @@
 <div class="navbar navbar-inverse" role="navigation">
     <div class="container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
             <a class="navbar-brand" href="/">HUSTACM</a>
         </div>
         <div class="collapse navbar-collapse">
