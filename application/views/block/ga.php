8ed70cb854ad87ae8153e6068249272970bb5aa3
He Jun <freefcw@gmail.com>
2014-01-25 17:15:49 +0800

    update new ga code

--- application/views/block/ga.php
+++ application/views/block/ga.php
@@ -1,18 +1,24 @@
 <?php $gacode = Model_Option::get_option('ga_code');
 if ( isset($gacode) ):?>
-<script type="text/javascript">
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', '<?php echo($gacode);?>']);
-    _gaq.push(['_setSiteSpeedSampleRate', 100]);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-        if ( $('script[id=gajs]').length == 0 )
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();
+        var scripts = s.getElementsByTagName(o);
+        var already_have = false;
+        for(item in scripts)
         {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.id = 'gascript';
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            try{
+                if ( scripts[item].src.indexOf(g) >= 0 ) already_have = true;
+            } catch (e){}
         }
-    })();
+        if ( ! already_have )
+        {
+            a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        }
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-6733942-1', 'hust.edu.cn');
+    ga('send', 'pageview');
+
 </script>
 <?php endif;?>
