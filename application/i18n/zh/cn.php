7f392b52a39bc3bb821da77d24948094c2760a18
Jason Lau <i@dotkrnl.com>
2014-03-13 00:16:47 +0800

    top_frontend translation for zh-cn

---
+++ application/i18n/zh/cn.php
@@ -0,0 +1,20 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+ 
+return array(
+  
+    'top_frontend.home' => '主页',
+    'top_frontend.problem' => '问题',
+    'top_frontend.status' => '状态',
+    'top_frontend.rank' => '排名',
+    'top_frontend.contest' => '比赛',
+    'top_frontend.faqs' => 'FAQ',
+    'top_frontend.discuss' => '讨论',
+    'top_frontend.more' => '更多',
+    'top_frontend.useredit' => '修改',
+    'top_frontend.mail' => '邮件',
+    'top_frontend.adminctl' => '管理',
+    'top_frontend.login' => '登入',
+    'top_frontend.logout' => '登出',
+    'top_frontend.register' => '注册',
+    
+);
