e39830c6193d5bf63e2abd02acd3c6cc81450181
Jason Lau <i@dotkrnl.com>
2014-03-16 16:10:26 +0800

    move init_i18n to Controller_Base and i18n tools to I18n

--- application/classes/I18n.php
+++ application/classes/I18n.php
@@ -16,6 +16,77 @@ class I18n extends Kohana_I18n {
         return isset($table[$string]) ? $table[$string] :
               (isset($alternative[$string]) ? $alternative[$string] : $string);
     }
+
+    /**
+     * Get user lang from session
+     *
+     * @param array $lang_supported language supported
+     *
+     * @return string language got from session or NULL
+     */
+    private static function get_user_lang($lang_supported) {
+        // Get session language, TODO: use user settings instead
+        $session = Session::instance();
+        $lang_current = $session->get('lang');
+        return in_array($lang_current, $lang_supported) ? $lang_current : NULL;
+    }
+
+    /**
+     * Set user lang to session
+     *
+     * @param string $lang_current language to set
+     */
+    private static function set_user_lang($lang_current) {
+        // Set session default, TODO: user instead
+        $session = Session::instance();
+        $session->set('lang', $lang_current);
+    }
+
+    /**
+     * Get user lang from Accepted-Language
+     *
+     * @param array $lang_supported language supported
+     *
+     * @return string language got from Accepted-Language or NULL
+     */
+    private static function get_browser_lang($lang_supported) {
+        // Get lang from Request::accept_lang().
+        $lang_current = NULL;
+        foreach (Request::accept_lang() as $key => $val)
+        {
+            if (in_array($key, $lang_supported))
+            {
+                $lang_current = $key;
+                break;
+            }
+        }
+        return $lang_current;
+    }
+
+    /**
+     * Initialize internationalization
+     * for current request
+     */
+    public static function init_i18n() {
+        // Get language config variables
+        $lang_settings = Kohana::$config->load('multilang')->get('languages', array());
+        $lang_supported = array_keys($lang_settings['supported']);
+
+        // Get language
+        $lang_current = self::get_user_lang($lang_supported);
+        if (! $lang_current) $lang_current = self::get_browser_lang($lang_supported);
+        if (! $lang_current) $lang_current = $lang_settings['default'];
+
+        // Set user language
+        self::set_user_lang($lang_current);
+
+        // Set request language and locale
+        $lang_config = $lang_settings['supported'][$lang_current];
+        if (i18n::lang() != $lang_config['code']) {
+            i18n::lang($lang_config['code']);
+            setlocale(LC_ALL, $lang_config['locale']);
+        }
+    }
 }
 
 /**
