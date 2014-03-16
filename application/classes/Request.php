589e039847d096ba0c3c253aaadb34d7880ff8fd
Jason Lau <i@dotkrnl.com>
2014-03-16 12:04:16 +0800

    rearrange Request code

--- application/classes/Request.php
+++ application/classes/Request.php
@@ -2,59 +2,84 @@
 
 class Request extends Kohana_Request {
     /**
-     * Extend the instance method to force the presence of the lang identifier.
+     * Get user lang from session
+     * 
+     * @param array $lang_supported
      *
-     * Creates a new request object for the given URI.
+     * @return string language
+     */
+    private static function get_user_lang($lang_supported) {
+        // Get session language, TODO: use user settings instead
+        $session = Session::instance();
+        $lang_current = $session->get('lang');
+        return in_array($lang_current, $lang_supported) ? $lang_current : NULL;
+    }
+
+    /**
+     * Get user lang from session
+     * 
+     * @param string $lang_current
+     */
+    private static function set_user_lang($lang_current) {
+        // Set session default, TODO: user instead
+        $session = Session::instance();
+        $session->set('lang', $lang_current);
+    }
+
+    /**
+     * Get user lang from Accepted-Language
+     * 
+     * @param array $lang_supported
+     *
+     * @return string language
+     */
+    private static function get_browser_lang($lang_supported) {
+        // Get lang from Request::accept_lang().
+        $lang_current = NULL;
+        foreach (Request::accept_lang() as $key => $val)
+        {
+            if (in_array($key, $lang_supported))
+            {
+                $lang_current = $key;
+                break;
+            }
+        }
+        return $lang_current;
+    }
+
+    /**
+     * Initialize internationalization
+     */
+    private static function init_i18n() {
+        // Get language config variables
+        $lang_settings = Kohana::$config->load('multilang')->get('languages', array());
+        $lang_supported = array_keys($lang_settings['supported']);
+
+        // Get language
+        $lang_current = self::get_user_lang($lang_supported);
+        if (! $lang_current) $lang_current = self::get_browser_lang($lang_supported);
+        if (! $lang_current) $lang_current = $lang_settings['default'];
+
+        // Set user language
+        self::set_user_lang($lang_current);
+
+        // Set request language and locale
+        $lang_config = $lang_settings['supported'][$lang_current];
+        if (i18n::lang() != $lang_config['code']) {
+            i18n::lang($lang_config['code']);
+            setlocale(LC_ALL, $lang_config['locale']);
+        }
+    }
+
+    /**
+     * Extend the factory method to force the presence of the lang identifier.
      *
      * @return  Request
      */
     public static function factory($uri = TRUE, $client_params = array(), $allow_external = TRUE, $injected_routes = array())
     {
         $instance = parent::factory($uri, $client_params, $allow_external, $injected_routes);
-
-        $lang_settings = Kohana::$config->load('multilang')->get('languages', array());
-
-        // Get language config variables
-        $lang_default = $lang_settings['default'];
-        $lang_supported = array_keys($lang_settings['supported']);
-
-        // Get session language, TODO: use user settings instead
-        $session = Session::instance();
-        $lang_current = $session->get('lang');
-
-        // Fallback to none
-        if (! in_array($lang_current, $lang_supported))
-        {
-            $lang_current = NULL;
-        }
-
-        if (! $lang_current) {
-            // Get lang from Request::accept_lang().
-            foreach (Request::accept_lang() as $key => $val)
-            {
-                if (in_array($key, $lang_supported))
-                {
-                    $lang_current = $key;
-                    break;
-                }
-            }
-        }
-
-        // Get lang from default
-        if (! $lang_current) $lang_current = $lang_default;
-
-        // Set session default, TODO: user instead
-        if ($session->get('lang') != $lang_current) $session->set('lang', $lang_current);
-
-        // get config from lang_current
-        $lang_config = $lang_settings['supported'][$lang_current];
-
-        // Set request language and locale
-        if (i18n::lang() != $lang_config['code']) {
-            i18n::lang($lang_config['code']);
-            setlocale(LC_ALL, $lang_config['locale']);
-        }
-
+        self::init_i18n();
         return $instance;
     }
 }
