d565d79878a2448256b365bc8789e619887ed2e1
He Jun <freefcw@gmail.com>
2014-03-16 10:47:38 +0800

    fix phpdoc in i18n, Change \r\n to \r

--- application/classes/I18n.php
+++ application/classes/I18n.php
@@ -2,30 +2,37 @@
 
 class I18n extends Kohana_I18n {
     /**
-      * @param   string   text to translate
-      * @param   string   target language
-      * @return  string
-      */
-    public static function get($string, $lang = NULL) {
-        if ( ! $lang)
+     * @param string $text text to translate
+     * @param string $lang
+     *
+     * @return  string
+     */
+    public static function get($text, $lang = NULL)
+    {
+        if ( !$lang )
         {
             $lang = I18n::$lang;
         }
-        $table = I18n::load($lang);
+        $table       = I18n::load($lang);
         $alternative = I18n::load('en-us');
-        return isset($table[$string]) ? $table[$string] :
-              (isset($alternative[$string]) ? $alternative[$string] : $string);
+
+        return isset($table[$text]) ? $table[$text] :
+            (isset($alternative[$text]) ? $alternative[$text] : $text);
     }
 }
 
 /**
-  * @uses    I18n::get
-  * @param   string  text to translate
-  * @param   array   values to replace in the translated text
-  * @param   string  source language (ignore here for no source language)
-  * @return  string
-  */
-function __($string, array $values = NULL, $source = NULL) {
+ * @uses     I18n::get
+ *
+ * @param text  $string
+ * @param array $values $array to replace in the translated text
+ * @param       string  text to translate
+ *
+ * @return  string
+ */
+function __($string, array $values = NULL, $source = NULL)
+{
     $string = I18n::get($string);
+
     return empty($values) ? $string : strtr($string, $values);
 }
