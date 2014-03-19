7652721e677cece32bae5b099ff96ea84409d3e2
Jason Lau <i@dotkrnl.com>
2014-03-19 15:33:00 +0800

    theme support

---
+++ application/config/themes.php
@@ -0,0 +1,25 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+
+    'themes' => array(
+
+        'supported' => array(
+
+            'bootstrap' => 'Bootstrap',
+            'amelia'    => 'Amelia',
+            'cerulean'  => 'Cerulean',
+            'darkly'    => 'Darkly',
+            'flatly'    => 'Flatly',
+            'readable'  => 'Readable',
+            'shamrock'  => 'Shamrock',
+            'simplex'   => 'Simplex',
+            'slate'     => 'Slate',
+            'united'    => 'United',
+
+        ),
+
+        'default' => 'bootstrap',
+    ),
+
+);
