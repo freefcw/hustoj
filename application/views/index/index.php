d0c29ff31267a0607fe558b0f3189a6063ea1ff8
He Jun <freefcw@gmail.com>
2014-10-09 12:17:32 +0800

    首页添加portal到qiming和vjudge

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -4,20 +4,35 @@
     <p><?php echo(__("index.any_problem")); ?></p>
 </div>
 <?php endif;?>
-<div class="row index">
-    <div class="news">
-        <?php /* @var Model_News[] $news_list */foreach($news_list as $news):?>
-        <div class="news-item">
-            <h4><a href="<?php e::url("/news/{$news->news_id}");?>"><?php echo($news->title);?></a><?php if ($news->is_top_news()):?><span class="label label-danger">top</span><?php endif;?></h4>
-            <div class="news-content readability">
-                <?php if ( mb_strlen($news->content) > 150 ):?>
-                <?php echo(mb_substr($news->content, 0, 100));?><a href="<?php e::url("/news/{$news->news_id}");?>" class="readmore">...</a>
-                <?php else:?>
-                <?php echo($news->content);?>
-                <?php endif;?>
-            </div>
+<?php
+if ( Request::current()->query('goto') == '1' )
+{
+    Cookie::set('hustoj', 1);
+}
+if ( Cookie::get('hustoj', 0) == '1' ):?>
+    <div class="row index">
+        <div class="news">
+            <?php /* @var Model_News[] $news_list */foreach($news_list as $news):?>
+                <div class="news-item">
+                    <h4><a href="<?php e::url("/news/{$news->news_id}");?>"><?php echo($news->title);?></a><?php if ($news->is_top_news()):?><span class="label label-danger">top</span><?php endif;?></h4>
+                    <div class="news-content readability">
+                        <?php if ( mb_strlen($news->content) > 150 ):?>
+                            <?php echo(mb_substr($news->content, 0, 100));?><a href="<?php e::url("/news/{$news->news_id}");?>" class="readmore">...</a>
+                        <?php else:?>
+                            <?php echo($news->content);?>
+                        <?php endif;?>
+                    </div>
+                </div>
+            <?php endforeach;?>
         </div>
-        <?php endforeach;?>
     </div>
-</div>
+<?php else:?>
+    <div id="front-guide">
+        <ul>
+            <li class="alert alert-success"><a href="/?goto=1">华中科技大学ACM竞赛系统</a> </li>
+            <li class="alert alert-success"><a href="/qiming">启明学院课程系统</a></li>
+            <li class="alert alert-success"><a href="/vjudge">Virtual Judge系统</a></li>
+        </ul>
+    </div>
 <?php echo(View::factory('block/pager', array('base_url' => '', 'total' => $total)));?>
+<?php endif;?>
